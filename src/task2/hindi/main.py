8925c0e29b4068386a7a20f151f0cebf5b2948bc
shahrukhx01 <sk28671@gmail.com>
2021-02-25 18:54:44 +0100

    add code

---
+++ src/task2/hindi/main.py
@@ -0,0 +1,24 @@
+from data import HASOCData
+from model import HindiLSTMClassifier
+from train import train_model, forward_pass
+import torch
+import pickle
+from config import config_dict
+from torch import nn
+from sklearn.metrics import classification_report, confusion_matrix, accuracy_score
+
+def main():
+    data = HASOCData(config_dict['file_paths'])
+    with open(config_dict['file_paths']['embeddings_path'], 'rb') as f:
+        embedding_weights = pickle.load(f)
+    print('embed',embedding_weights.T.shape, 'expect', (len(data.vocab), 300))
+    model = HindiLSTMClassifier(batch_size=config_dict['batch_size'], output_size=config_dict['num_classes'], vocab_size=len(data.vocab), \
+                                hidden_size=32, embedding_size=300, weights=torch.FloatTensor(embedding_weights.T),\
+                                lstm_layers=2)
+    optimizer = torch.optim.Adam(filter(lambda p: p.requires_grad, model.parameters()))
+    train_model(model, optimizer, data, config_dict['batch_size'], max_epochs=5)
+
+
+
+if __name__ == "__main__":
+    main()
